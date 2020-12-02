b60273c7f6b696652162e15e9e996390c7de6240
astrahl008 <alexanderstrahl@hotmail.com>
2020-12-01 21:25:23 -0500

    First Step towards light modes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Light Modes</h1>
-    </div>
-  );
+class App extends Component {
+  
+  constructor(props){
+    super(props)
+    this.state = {
+      darkMode: false
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className={this.state.darkMode ? 'darkMode' : ''}>
+          <h1 >Light Modes</h1>
+
+          <button onClick={this.darkMode}>Dark</button>
+        </div>
+      </div>
+    )};
+
+    darkMode = () => {
+      this.setState({darkMode: !this.state.darkMode});
+    }
 }
 
 export default App;
